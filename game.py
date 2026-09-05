a65ddfdae500bcda89207e85b8f228ab3fdaafa6
Alisa Tkachenko <155114764+AlsoLitmus@users.noreply.github.com>
2026-09-04 17:24:52 -0700

    Improve user input validation and handling
    Refactor user input handling to ensure integer input and separate input retrieval into its own function.

--- game.py
+++ game.py
@@ -1,19 +1,25 @@
 # Python number guessing game
 import random
 
+# Check if the user input is an integer.
 def check_if_integer(user_input):
   if isinstance(user_input):
-    return
+    return user_input
   else:
-    print("You did not enter a number from 1 to 10.")
-    number = input("Try again: ")
-    return
+    while !isinstance(user_input):
+      print("You did not enter an integer Try again.")
+      grab_user_input(user_input)
+    return user_input
 
-
-number = input("Guess a number from 1 to 10: ")
+# This number will recieve user input and assign it to number.
+def grab_user_input(number):
+  number = input("Guess a number from 1 to 10: ")
+  return number
+  
+number = grab_user_input(number)
 
 # Check if user input is an integer.
-
+number = check_if_integer()
 
 # Generate a random number.
 random_integer = random.randint(1, 10)
