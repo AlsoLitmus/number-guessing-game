f37a36d79ba22a0a4468b07d0b088f69aa24ba41
Alisa Tkachenko <itsalisatkach@gmail.com>
2026-09-05 18:38:01 -0700

    Tested the program.

--- game.py
+++ game.py
@@ -1,47 +1,48 @@
 # Python number guessing game
 import random
 
-# Check if the user input is an integer.
-def check_if_integer(user_input):
-  if isinstance(user_input):
-    return user_input
-  else:
-    while !isinstance(user_input):
-      print("You did not enter an integer Try again.")
-      user_input = grab_user_input(user_input)
-    return user_input
-
 # This number will recieve user input and assign it to number.
 def grab_user_input(number):
   number = input("Guess a number from 1 to 10: ")
   return number
 
 def check_if_in_range(num):
-  if num > -1 and num < 11:
+  if int(num) > -1 and int(num) < 11:
     return num
   else:
-    while num < 0 or num > 11:
+    while int(num) < 0 or int(num) > 11:
       print("The guessed number is out of range. Guess again.")
       num = grab_user_input(num)
     return num
-  
-number = grab_user_input(number)
 
-# Check if user input is an integer.
-number = check_if_integer(number)
+def convert_to_integer(num):
+  try:
+    num = int(num)
+  except ValueError:
+    print("You did not enter an integer. Try again.")
+    num = grab_user_input(num)
+  return num
+
+number = ""
+number = grab_user_input(number)
 number = check_if_in_range(number)
 
 # Generate a random number.
 random_integer = random.randint(1, 10)
 
 # Check if the number guessed is greater than, less than, or equal to random_integer.
-while number != random_integer:
-  if number < random_integer:
+while number != int(random_integer):
+  number = convert_to_integer(number)
+  if int(number) < random_integer:
     number = input("The number is too low! Guess again: ")
-  else if number > random_integer:
+    number = convert_to_integer(number)
+    number = check_if_in_range(number)
+  elif number > int(random_integer):
     number = input("The number is too high! Guess again: ")
-  
-if number == random_integer:
+    number = convert_to_integer(number)
+    number = check_if_in_range(number)
+
+if number == int(random_integer):
   print("You have guessed the number correctly!")
   
   
